3f7cd6e604bea9fd3cf0124814fef1a7bf75ee9c
Yourtion <yourtion@gmail.com>
2014-07-27 16:35:48 +0800

    Initial Commit

---
+++ LineAnimationDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LineAnimationDemo
+//
+//  Created by YourtionGuo on 7/27/14.
+//  Copyright (c) 2014 yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LineAnimationDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LineAnimationDemo
+//
+//  Created by YourtionGuo on 7/27/14.
+//  Copyright (c) 2014 yourtion. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
